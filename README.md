c99af2de4f2d301563aecde2b817798ca1d84c84
Rob Ellingson <76190447+awolrob@users.noreply.github.com>
2021-06-29 22:35:43 -0400

    Test Google Map API

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# Project1
+# Project1 - Attempt Google Map API Calls
+
+The longitude and latitude are hard coded in the js code (line 2) for Australia
+
+using google Maps for JAvascript https://developers.google.com/maps/documentation/javascript/overview?hl=en_US
+
+In theory we can learn the API query requirements to send the API a list of PIN points to label and display.
---
+++ map.js
@@ -0,0 +1,20 @@
+function initMap() {
+    const myLatLng = { lat: 41.38855173798833, lng: -82.21036065433786 };
+    const myLatLng2 = { lat: 41.385959371762645, lng: -82.21088860697019 }; 
+    /*41.38923980160808, -82.21027917718673  41.38855173798833, -82.21036065433786*/
+    // 41.385959371762645, -82.21088860697019
+    const map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 15,
+        center: myLatLng,
+    });
+    new google.maps.Marker({
+        position: myLatLng,
+        map,
+        title: "DQ In Amherst",
+    });
+    new google.maps.Marker({
+        position: myLatLng2,
+        map,
+        title: "Subway",
+    });
+}
