32693da0ad0ccb7c76f1b6dfc3288c7183a102ec
Rob Ellingson <76190447+awolrob@users.noreply.github.com>
2021-07-04 09:51:09 -0400

    delete map.js add states.js

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,3 +1,4 @@
+/* variables */
 var searchBtn = document.getElementById("user-form");
 var searchState = document.getElementById("state");
 var searchTerm;
@@ -6,8 +7,8 @@ var searchTerm;
 var aNPS = [
     {
         markerName: "center",
-        latitude: "41.38855173798833",
-        longitude: "-82.21036065433786",
+        latitude: "39.50",
+        longitude: "-98.35",
     }
 ];
 
@@ -23,15 +24,15 @@ searchClickHandler = function (event) {
     event.preventDefault();
     searchTerm = searchState.value.trim();
     console.log(searchTerm)
-    fNpsApi();
+    fNpsApi(searchTerm);
 };
 
 //call National Park Service API for state code selected
-fNpsApi = function () {
+fNpsApi = function (stateIn) {
     var sNpsApi = 'https://developer.nps.gov/api/v1/campgrounds?' +
         'stateCode=' +
-        searchTerm +
-        '&limit=500' +
+        stateIn +
+        '&limit=700' +
         '&api_key=nNVtVCtPxYIdqgXcnAhfGjtCq9cW1SUGJ6AnV68u';
 
     // Create a variable to hold the value of rating
@@ -63,8 +64,57 @@ fNpsApi = function () {
 
 }
 
+function initMap() {
+    myLatLng.lat = parseFloat(aNPS[0].latitude);
+    myLatLng.lng = parseFloat(aNPS[0].longitude);
+    map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 4,
+        center: myLatLng,
+    });
+}
+
+function updateMap() {
+    const image =
+        "./assets/images/clipart2984201.png";
+    myLatLng.lat = parseFloat(aNPS[0].latitude);
+    myLatLng.lng = parseFloat(aNPS[0].longitude);
+    const map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 5,
+        center: myLatLng,
+    });
+    new google.maps.Marker({
+        position: myLatLng,
+        map,
+        title: aNPS[0].markerName,
+    });
+
+    for (i = 1; i < aNPS.length; i++) {
+        myLatLng.lat = parseFloat(aNPS[i].latitude);
+        myLatLng.lng = parseFloat(aNPS[i].longitude);
+        markerName = aNPS[i].markerName;
+        // window.setTimeout(() => {
+// add info window?  https://developers.google.com/maps/documentation/javascript/examples/infowindow-simple
+
+        new google.maps.Marker({
+            position: myLatLng,
+            map,
+            title: markerName,
+            icon: image,
+            animation: google.maps.Animation.DROP,
+        })
+            ;
+        // }, 50);
+    }
+
+}
+
+/* MAIN LOGIC*/
+//Center map in US//
+// initMap(parseFloat(aNPS[0].latitude),parseFloat(aNPS[0].longitude),3);
+//Call NPS API for all sites in the US
+fNpsApi("");
+
 searchBtn.addEventListener("submit", searchClickHandler);
-/* variables */
 // Define Variables to hold Camp API data
 //  location data, name data, long lat
 // Define Variables to hold Weather API data
