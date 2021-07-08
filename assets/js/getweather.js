7857463a1560e1449ba0704f69b25dfd818ba4b0
Rob Ellingson <76190447+awolrob@users.noreply.github.com>
2021-07-07 20:21:59 -0400

    add locic for weather etc.

--- assets/js/getweather.js
+++ assets/js/getweather.js
@@ -1,3 +1,22 @@
-var getweather = function(lat, lng) {
-    
-}
+var getCurWeather = function (lat, lng) {
+    var apiUrlonecall = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat +
+        "&lon=" + lng +
+        "&exclude=minutely,hourly,alerts" +
+        "&units=imperial" +
+        "&appid=f0fb5a2fd74295d57b15c5c4bd25d82f";
+    fetch(apiUrlonecall)
+        .then(function (response) {
+            // request was successful
+            if (response.ok) {
+                response.json().then(function (data) {
+                    loadUI(data.current, data.daily);
+                });
+            } else {
+                alert("Error: " + response.statusText);
+            }
+        })
+        .catch(function (error) {
+            alert("Unable to connect to Open Weather Map API One Call");
+        });
+
+};
